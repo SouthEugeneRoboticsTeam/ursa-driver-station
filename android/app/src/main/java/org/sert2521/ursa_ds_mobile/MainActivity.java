bdf33681c1bb388a764c4fef2fcf81f5a0447b20
Andrew Dassonville <dassonville.andrew@gmail.com>
2019-06-23 21:37:14 -0700

    Initial commit

---
+++ android/app/src/main/java/org/sert2521/ursa_ds_mobile/MainActivity.java
@@ -0,0 +1,13 @@
+package org.sert2521.ursa_ds_mobile;
+
+import android.os.Bundle;
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+  }
+}
