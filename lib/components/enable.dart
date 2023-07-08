SouthEugeneRoboticsTeam/ursa-driver-station

commit bf6c282d72d16256b759cc8d33aa56ee1be4a574

Fix overriding of robot automatic disables
Fixes https://github.com/SouthEugeneRoboticsTeam/ursa-driver-station/issues/10

lib/components/enable.dart
@@ -41,6 +41,19 @@ class SlideToEnableState extends State<SlideToEnable>
   late Animation<double> opacityAnimation;
 
   @override
+  void didUpdateWidget(SlideToEnable oldWidget) {
+    super.didUpdateWidget(oldWidget);
+
+    if (widget.enabled != oldWidget.enabled) {
+      if (widget.enabled) {
+        _dragController.forward();
+      } else {
+        _dragController.reverse();
+      }
+    }
+  }
+
+  @override
   void initState() {
     super.initState();
 

lib/domain/connection.dart
@@ -18,8 +18,6 @@ RawDatagramSocket? socket;
 Timer? sendLoop;
 StreamSubscription? subscription;
 
-const interval = Duration(milliseconds: 50); // 20Hz refresh
-
 Future initConnection() async {
   Connectivity()
       .onConnectivityChanged
@@ -61,7 +59,7 @@ void initSendLoop() {
   sendLoop?.cancel();
 
   sendLoop = Timer.periodic(
-      interval,
+      const Duration(milliseconds: 50),
       (Timer t) => SchedulerBinding.instance
           .scheduleTask(() => sendData(commandMessage.getBytes()), Priority.touch));
 }
@@ -114,6 +112,10 @@ void setPidCommand(double angleP, double angleI, double angleD, double speedP, d
 void telemetryMessageListener(TelemetryMessage message) {
   TelemetryModel().setTelemetry(message);
   ConnectionModel().setStatus(ConnectionStatus.connected);
+
+  if (message.enabled != commandMessage.enabled) {
+    setEnabledCommand(message.enabled);
+  }
 
   // Expect a message at least every 2 seconds
   connectionTimeoutTimer?.cancel();

commit 3791152107e7ba99b2151aa8f71191e73bd48066

Fix race condition when enabling
Bug introduced in bf6c282d72d16256b759cc8d33aa56ee1be4a574

lib/components/enable.dart
@@ -45,6 +45,7 @@ class SlideToEnableState extends State<SlideToEnable>
     super.didUpdateWidget(oldWidget);
 
     if (widget.enabled != oldWidget.enabled) {
+      _enabled = widget.enabled;
       if (widget.enabled) {
         _dragController.forward();
       } else {

lib/domain/connection.dart
@@ -2,7 +2,6 @@ import 'dart:async';
 import 'dart:io';
 
 import 'package:connectivity_plus/connectivity_plus.dart';
-import 'package:flutter/scheduler.dart';
 import 'package:flutter_joystick/flutter_joystick.dart';
 
 import '../models/connection_model.dart';
@@ -12,11 +11,15 @@ import 'dtos/telemetry_message.dart';
 import 'networking.dart';
 
 CommandMessage commandMessage = CommandMessage();
+CommandMessage _previousCommandMessage = CommandMessage();
+
 Timer? connectionTimeoutTimer;
 
 RawDatagramSocket? socket;
 Timer? sendLoop;
 StreamSubscription? subscription;
+
+DateTime? lastMessageSend;
 
 Future initConnection() async {
   Connectivity().onConnectivityChanged.listen(connectivityChangedListener);
@@ -59,11 +62,29 @@ void initSendLoop() {
   sendLoop?.cancel();
 
   sendLoop = Timer.periodic(
-    const Duration(milliseconds: 50),
-    (Timer t) => SchedulerBinding.instance.scheduleTask(
-      () => sendData(commandMessage.getBytes()),
-      Priority.touch,
-    ),
+    const Duration(milliseconds: 10),
+    (Timer t) {
+      // Send message if...
+      //   1. The message has changed AND we haven't sent a message in the past 20ms, OR
+      //   2. We haven't sent a message in the past 500ms
+      //
+      // TODO: We should replace #2 with a heartbeat message
+
+      bool messageChanged = commandMessage != _previousCommandMessage;
+      bool messageSentRecently = lastMessageSend != null &&
+          lastMessageSend!.isAfter(
+              DateTime.now().subtract(const Duration(milliseconds: 20)));
+      bool messageSentLongAgo = lastMessageSend == null ||
+          lastMessageSend!.isBefore(
+              DateTime.now().subtract(const Duration(milliseconds: 500)));
+
+      if ((messageChanged && !messageSentRecently) || messageSentLongAgo) {
+        sendData(commandMessage.getBytes());
+
+        lastMessageSend = DateTime.now();
+        _previousCommandMessage = CommandMessage.from(commandMessage);
+      }
+    },
   );
 }
 
@@ -124,7 +145,8 @@ void telemetryMessageListener(TelemetryMessage message) {
   TelemetryModel().setTelemetry(message);
   ConnectionModel().setStatus(ConnectionStatus.connected);
 
-  if (message.enabled != commandMessage.enabled) {
+  // If the enabled state has changed, update the UI
+  if (message.enabled != _previousCommandMessage.enabled) {
     setEnabledCommand(message.enabled);
   }
 

lib/domain/dtos/command_message.dart
@@ -68,4 +68,61 @@ class CommandMessage {
 
     return _builder.takeBytes();
   }
+
+  static CommandMessage from(CommandMessage message) {
+    var commandMessage = CommandMessage();
+
+    commandMessage.enabled = message.enabled;
+    commandMessage.speed = message.speed;
+    commandMessage.turn = message.turn;
+    commandMessage.auxiliary = message.auxiliary;
+    commandMessage.advanced = message.advanced;
+    commandMessage.angleP = message.angleP;
+    commandMessage.angleI = message.angleI;
+    commandMessage.angleD = message.angleD;
+    commandMessage.speedP = message.speedP;
+    commandMessage.speedI = message.speedI;
+    commandMessage.speedD = message.speedD;
+    commandMessage.saveRecallState = message.saveRecallState;
+
+    return commandMessage;
+  }
+
+  @override
+  String toString() {
+    return 'CommandMessage{enabled: $enabled, speed: $speed, turn: $turn, auxiliary: $auxiliary, advanced: $advanced, angleP: $angleP, angleI: $angleI, angleD: $angleD, speedP: $speedP, speedI: $speedI, speedD: $speedD, saveRecallState: $saveRecallState}';
+  }
+
+  @override
+  bool operator ==(Object other) {
+    return other is CommandMessage &&
+        enabled == other.enabled &&
+        speed == other.speed &&
+        turn == other.turn &&
+        auxiliary == other.auxiliary &&
+        advanced == other.advanced &&
+        angleP == other.angleP &&
+        angleI == other.angleI &&
+        angleD == other.angleD &&
+        speedP == other.speedP &&
+        speedI == other.speedI &&
+        speedD == other.speedD &&
+        saveRecallState == other.saveRecallState;
+  }
+
+  @override
+  int get hashCode => Object.hash(
+        enabled,
+        speed,
+        turn,
+        auxiliary,
+        advanced,
+        angleP,
+        angleI,
+        angleD,
+        speedP,
+        speedI,
+        speedD,
+        saveRecallState,
+      );
 }

lib/domain/networking.dart
@@ -1,13 +1,15 @@
 import 'dart:io';
 
 final InternetAddress robotAddress = InternetAddress('10.25.21.1');
+// final InternetAddress robotAddress = InternetAddress('10.0.0.244');
 const int robotPort = 2521;
 
 void logNetworkInterfaces() async {
   for (var interface in await NetworkInterface.list()) {
     print('== Interface: ${interface.name} ==');
     for (var addr in interface.addresses) {
-      print('${addr.address} ${addr.host} ${addr.isLoopback} ${addr.rawAddress} ${addr.type.name}');
+      print(
+          '${addr.address} ${addr.host} ${addr.isLoopback} ${addr.rawAddress} ${addr.type.name}');
     }
   }
 }